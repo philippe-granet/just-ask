e6ab0b433797f8877788840923c20456d70a7793
Philippe GRANET <philippe.granet@gmail.com>
2017-09-05 02:19:47 +0200

    Fix issues

--- src/main/java/com/rationaleemotions/servlets/HubRequestsProxyingServlet.java
+++ src/main/java/com/rationaleemotions/servlets/HubRequestsProxyingServlet.java
@@ -1,9 +1,8 @@
 package com.rationaleemotions.servlets;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.openqa.grid.internal.Registry;
 import org.openqa.grid.web.servlet.RegistryBasedServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.rationaleemotions.grid.client.RequestForwardingClient;
@@ -18,76 +19,81 @@ import com.rationaleemotions.grid.client.RequestForwardingClientProvider;
 import com.rationaleemotions.grid.session.SeleniumSessions;
 
 /**
- * @author IgorV
- *         Date: 13.2.2017
+ * @author IgorV Date: 13.2.2017
  */
 public class HubRequestsProxyingServlet extends RegistryBasedServlet {
 
-    private static final Logger LOGGER = Logger.getLogger(HubRequestsProxyingServlet.class.getName());
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    @VisibleForTesting
-    RequestForwardingClientProvider requestForwardingClientProvider;
+	@VisibleForTesting
+	static RequestForwardingClientProvider requestForwardingClientProvider = new RequestForwardingClientProvider();
 
-    public HubRequestsProxyingServlet() {
-        this(null);
-    }
+	public HubRequestsProxyingServlet() {
+		this(null);
+	}
 
-    public HubRequestsProxyingServlet(Registry registry) {
-        super(registry);
-        requestForwardingClientProvider = new RequestForwardingClientProvider();
-    }
+	public HubRequestsProxyingServlet(Registry registry) {
+		super(registry);
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forwardRequest(req, resp);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		forwardRequest(req, resp);
+	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		forwardRequest(req, resp);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forwardRequest(req, resp);
-    }
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		forwardRequest(req, resp);
+	}
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forwardRequest(req, resp);
-    }
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		forwardRequest(req, resp);
+	}
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forwardRequest(req, resp);
-    }
+	private void forwardRequest(HttpServletRequest req, HttpServletResponse resp) {
+		RequestForwardingClient requestForwardingClient;
+		try {
+			requestForwardingClient = createExtensionClient(req.getPathInfo());
+		} catch (IllegalArgumentException e) {
+			try {
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			} catch (IOException e1) {
+				LOGGER.error("Exception during sendError", e1);
+			}
+			return;
+		}
 
-    private void forwardRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        RequestForwardingClient requestForwardingClient;
-        try {
-            requestForwardingClient = createExtensionClient(req.getPathInfo());
-        } catch (IllegalArgumentException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-            return;
-        }
+		try {
+			requestForwardingClient.forwardRequest(req, resp);
+		} catch (IOException e) {
+			LOGGER.error("Exception during request forwarding", e);
+			try {
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			} catch (IOException e1) {
+				LOGGER.error("Exception during sendError", e1);
+			}
+		}
+	}
 
-        try {
-            requestForwardingClient.forwardRequest(req, resp);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception during request forwarding", e);
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-        }
-    }
+	private RequestForwardingClient createExtensionClient(String path) {
+		LOGGER.info("Forwarding request with path: {}", path);
+		String sessionId = SeleniumSessions.getSessionIdFromPath(path);
+		LOGGER.info("Retrieving remote host for session: {}", sessionId);
 
-    private RequestForwardingClient createExtensionClient(String path) {
-        LOGGER.info("Forwarding request with path: " + path);
-        String sessionId = SeleniumSessions.getSessionIdFromPath(path);
-        LOGGER.info("Retrieving remote host for session: " + sessionId);
+		SeleniumSessions sessions = new SeleniumSessions(getRegistry());
+		sessions.refreshTimeout(sessionId);
 
-        SeleniumSessions sessions = new SeleniumSessions(getRegistry());
-        sessions.refreshTimeout(sessionId);
+		URL remoteHost = sessions.getRemoteHostForSession(sessionId);
+		String host = remoteHost.getHost();
+		int port = remoteHost.getPort();
+		LOGGER.info("Remote host retrieved: {}:{}", host, port);
 
-        URL remoteHost = sessions.getRemoteHostForSession(sessionId);
-        String host = remoteHost.getHost();
-        int port = remoteHost.getPort();
-        LOGGER.info("Remote host retrieved: " + host + ":" + port);
-
-        return requestForwardingClientProvider.provide(host, port);
-    }
+		return requestForwardingClientProvider.provide(host, port);
+	}
 }
